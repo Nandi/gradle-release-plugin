505348a38c1c08ebc366c4ac8505ca4061c8046b
Simon Nandi Waage Bakkevig <simon@headlessideas.com>
2021-08-02 18:28:13 +0000

    GRP-12 Update dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,17 +1,17 @@
 object Versions {
-    const val KTLINT = "0.41.0"
+    const val KTLINT = "0.42.0"
     const val JUNIT = "5.7.2"
     const val RECKON = "0.13.0"
-    const val JGIT = "5.11.1.202105131744-r"
+    const val JGIT = "5.12.0.202106070339-r"
 }
 
 object BuildPluginsVersion {
-    const val DETEKT = "1.17.0"
-    const val KOTLIN = "1.5.0"
-    const val KTLINT = "10.0.0"
-    const val VERSIONS_PLUGIN = "0.38.0"
+    const val DETEKT = "1.17.1"
+    const val KOTLIN = "1.5.21"
+    const val KTLINT = "10.1.0"
+    const val VERSIONS_PLUGIN = "0.39.0"
     const val RECKON_PLUGIN = "0.13.0"
-    const val PLUGIN_PUBLISH = "0.14.0"
+    const val PLUGIN_PUBLISH = "0.15.0"
 }
 
 object VersioningLib {
